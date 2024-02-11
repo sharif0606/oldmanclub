94f8b0c1ef0d5a6420c7045897f3f05b3b227dd9
tawhid12 <tawhid99@gmail.com>
2024-02-11 23:46:54 +0600

    change

--- resources/views/user/nfc-card/index.blade.php
+++ resources/views/user/nfc-card/index.blade.php
@@ -3,69 +3,56 @@
 @section('content')
     <!-- Bordered table start -->
     <div class="row">
-        <div class="col-12">
+        <div class="col-3">
             <div class="card">
-                <!-- table bordered -->
-                <div class="table-responsive">
-                    <div>
-                        <a class="pull-right fs-1" href="{{ route('nfc_card.create') }}"><i class="fa fa-plus"></i></a>
-                        <a class="pull-right fs-1" href="{{ route('nfc_card.index') }}">NFC Card</a>
+                <a href="{{ route('nfc_card.create') }}">
+                    <div class="card-body">
+                        <i class="fa fa-plus"></i>
+                        Add New
                     </div>
-                    <table class="table table-bordered mb-0" id="phone_book">
-                        <thead>
-                            <tr>
-                                <th scope="col">{{ __('#SL') }}</th>
-                                <th scope="col">{{ __('Card Name') }}</th>
-                                <th scope="col">{{ __('Card Type') }}</th>
-                                <th scope="col">{{ __('Qr Code') }}</th>
-                                <th class="white-space-nowrap">{{ __('Action') }}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {{-- $nfc_cards --}}
-                            @forelse($nfc_cards as $value)
-                                <tr>
-                                    <th scope="row">{{ ++$loop->index }}</th>
-                                    <td>{{ __($value->card_name) }}</td>
-                                    <td>
-                                        @if ($value->card_type == 1)
-                                            {{ __('Wrok') }}
-                                        @else
-                                            {{ __('Personal') }}
-                                        @endif
-                                    </td>
-                                    <td>{!! QrCode::size(100)->generate(Request::url()) !!}</td>
-                                    <td class="white-space-nowrap">
-                                        @if ($value->status !== 2)
-                                            <a href="{{ route('nfc_card.show', encryptor('encrypt', $value->id)) }}">
-                                                <i class="fa fa-eye"></i>
-                                            </a>
-                                            <a href="{{ route('nfc_card.edit', encryptor('encrypt', $value->id)) }}">
-                                                <i class="fa fa-edit"></i>
-                                            </a>
-
-                                            <a href="javascript:void()" onclick="$('#form{{ $value->id }}').submit()">
-                                                <i class="fa fa-trash"></i>
-                                            </a>
-                                        @endif
-                                        <form id="form{{ $value->id }}"
-                                            action="{{ route('nfc_card.destroy', encryptor('encrypt', $value->id)) }}"
-                                            method="post">
-                                            @csrf
-                                            @method('delete')
-                                        </form>
-
-                                    </td>
-                                </tr>
-                            @empty
-                                <tr>
-                                    <th colspan="8" class="text-center">No NFC Card Found</th>
-                                </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
-                </div>
+                </a>
             </div>
         </div>
+    </div>
+    <div class="row">
+        @forelse ($nfc_cards as $nfc_card)
+            <div class="col-4">
+                <!-- Change the 'd' attribute to modify the shape -->
+                <path d="M0 -24H72V54H0V-24Z" fill="blue"></path>
+                <!-- Change the 'd' attribute to modify the shape -->
+                <!-- Change the 'fill' attribute to change the color -->
+                <path d="M72 72.5V39.18C44.16 29.9533 29.568 63.3176 0 41.7337V72.5H72Z" fill="red"></path>
+                    <a href="{{ route('nfc_card.create') }}">
+                        <svg viewBox="0 0 72 72" focusable="false" class="chakra-icon chakra-icon css-1uz6nvy" width="100%" height="100%" preserveAspectRatio="xMidYMid meet">
+                            <g clip-path="url(#clip0_1931_53838)">
+                               
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_1931_53838">
+                                    <rect fill="white" height="72" rx="16" width="72"></rect>
+                                </clipPath>
+                            </defs>
+                        </svg>
+                            <svg viewBox="0 0 72 72" focusable="false" class="chakra-icon chakra-icon"
+                                width="100%" height="200px" preserveAspectRatio="xMidYMid meet">
+                                <g clip-path="url(#clip0_1931_53838)">
+                                    <!-- Change the 'd' attribute to modify the shape -->
+                                    <path d="M0 -24H72V54H0V-24Z" fill="blue"></path>
+                                    <!-- Change the 'd' attribute to modify the shape -->
+                                    <!-- Change the 'fill' attribute to change the color -->
+                                    <path d="M72 72.5V39.18C44.16 29.9533 29.568 63.3176 0 41.7337V72.5H72Z" fill="red">
+                                    </path>
+                                </g>
+                                <defs>
+                                    {!! $nfc_card->card_design?->design_card?->svg !!}
+                                </defs>
+                            </svg>
+                       
+                    </a>
+              
+            </div>
+        @empty
+            No Card Found
+        @endforelse
     </div>
 @endsection
