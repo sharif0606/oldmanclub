60a9e33de21f9ecf4ff05983ede6b5fc1a341164
jasim uddin <jasimuddinm180@gmail.com>
2024-02-15 21:38:32 +0600

    printing service done

--- app/Http/Controllers/Backend/Printingservice/PrintingServiceImageController.php
+++ app/Http/Controllers/Backend/Printingservice/PrintingServiceImageController.php
@@ -32,27 +32,43 @@ class PrintingServiceImageController extends Controller
      */
     public function store(Request $request)
     {
-        try{
-            $print_image = new PrintingServiceImage;
-            $print_image->printing_service_id  = $request->printing_service_id;
-            if ($request->hasFile('image')) {
-                $imageName = rand(111,999) . time() . '.' .$request->image->extension();
-                $request->image->move(public_path('uploads/printimages'), $imageName);
-                $print_image->image = $imageName;
+        try {
+            $files = $request->file('image');
+            $is_featured_values = $request->input('is_featured', []); // Get is_featured values from the request or an empty array
+
+            $existing_featured_item = PrintingServiceImage::where('printing_service_id', $request->printing_service_id)
+                    ->where('is_featured', true)
+                    ->first();
+            if ($existing_featured_item) {
+                    $existing_featured_item->update(['is_featured' => false]);
             }
-            if ($request->has('is_featured')) {
-                $print_image->is_featured = $request->is_featured;
-            } else {
-                $print_image->is_featured = false;
+
+            foreach ($files as $index => $file) {
+                $print_image = new PrintingServiceImage;
+                $print_image->printing_service_id  = $request->printing_service_id;
+
+                if ($file->isValid()) {
+                    $imageName = rand(111, 999) . time() . '.' . $file->getClientOriginalExtension();
+                    $file->move(public_path('uploads/printimages'), $imageName);
+                    $print_image->image = $imageName;
+                }
+                
+                // Check if is_featured value exists for the current index
+                $is_featured = isset($is_featured_values[$index]) ? $is_featured_values[$index] : false;
+
+                $print_image->is_featured = $is_featured;
+                $print_image->created_by = currentUserId();
+
+                if ($print_image->save()) {
+                    $this->notice::success('Data Successfully saved');
+                } else {
+                    $this->notice::error('Failed to save data');
+                }
             }
-            $print_image->created_by = currentUserId();
-            if($print_image->save()){
-                $this->notice::success('Data Successfully saved');
-                return redirect()->route('print_service_image.index');
-            }
-            
-        } catch(Exception $e) {
-            // dd($e);
+
+            return redirect()->route('print_service_image.index');
+
+        } catch (Exception $e) {
             $this->notice::error('Please try again');
             return redirect()->back()->withInput();
         }
@@ -61,9 +77,9 @@ class PrintingServiceImageController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(PrintingServiceImage $printingServiceImage)
+    public function show($id)
     {
-        //
+        
     }
 
     /**
@@ -72,8 +88,8 @@ class PrintingServiceImageController extends Controller
     public function edit($id)
     {
         $print_service = PrintingService::get();
-        $print_image = PrintingServiceImage::findOrFail(encryptor('decrypt',$id));
-        return view('backend.print_service_image.edit',compact('print_image','print_service'));
+        $print_image = PrintingServiceImage::findOrFail(encryptor('decrypt', $id));
+        return view('backend.print_service_image.edit', compact('print_image', 'print_service'));
     }
 
     /**
@@ -81,41 +97,52 @@ class PrintingServiceImageController extends Controller
      */
     public function update(Request $request, $id)
     {
-        try{
-            $print_image = PrintingServiceImage::findOrFail(encryptor('decrypt',$id));
+        try {
+            $print_image = PrintingServiceImage::findOrFail(encryptor('decrypt', $id));
 
-            // Find any existing item that is currently marked as featured for the same printing_service_id
-            $existing_featured_item = PrintingServiceImage::where('printing_service_id', $request->printing_service_id)
+            // Find any existing items that are currently marked as featured for the same printing_service_id
+            $existing_featured_items = PrintingServiceImage::where('printing_service_id', $request->printing_service_id)
                 ->where('is_featured', true)
-                ->first();
+                ->get();
             
-            // Update the is_featured status of the existing featured item if it exists
-            if ($existing_featured_item) {
+            // Update the is_featured status of the existing featured items if they exist
+            foreach ($existing_featured_items as $existing_featured_item) {
                 $existing_featured_item->update(['is_featured' => false]);
             }
-            $print_image->printing_service_id  = $request->printing_service_id;
-            if ($request->hasFile('image')) {
-                $imageName = rand(111,999) . time() . '.' .$request->image->extension();
-                $request->image->move(public_path('uploads/printimages'), $imageName);
-                $print_image->image = $imageName;
+
+            $print_image->printing_service_id = $request->printing_service_id;
+            
+            // Process multiple images
+            if ($request->hasFile('images')) {
+                foreach ($request->file('images') as $file) {
+                    $imageName = rand(111, 999) . time() . '.' . $file->getClientOriginalExtension();
+                    $file->move(public_path('uploads/printimages'), $imageName);
+                    
+                    // Create a new PrintingServiceImage for each image
+                    $new_print_image = new PrintingServiceImage();
+                    $new_print_image->printing_service_id = $request->printing_service_id;
+                    $new_print_image->image = $imageName;
+                    
+                    // Determine if the image should be marked as featured
+                    $is_featured = $request->has('is_featured') && in_array($file->getClientOriginalName(), $request->is_featured);
+                    $new_print_image->is_featured = $is_featured;
+                    $new_print_image->created_by = currentUserId();
+                    $new_print_image->save();
+                }
             }
-            if ($request->has('is_featured')) {
-                $print_image->is_featured = $request->is_featured;
-            } else {
-                $print_image->is_featured = false;
-            }
+
             $print_image->updated_by = currentUserId();
-            if($print_image->save()){
+            
+            if ($print_image->save()) {
                 $this->notice::success('Data Successfully saved');
                 return redirect()->route('print_service_image.index');
             }
-            
-        } catch(Exception $e) {
-            // dd($e);
+        } catch (Exception $e) {
             $this->notice::error('Please try again');
             return redirect()->back()->withInput();
         }
     }
+
 
     /**
      * Remove the specified resource from storage.
