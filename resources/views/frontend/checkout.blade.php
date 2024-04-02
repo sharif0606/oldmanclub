4a561f04ac6d7f00a6d2e467fc39590f37131505
tawhid12 <tawhid99@gmail.com>
2024-04-02 20:50:58 +0600

    correction

--- resources/views/frontend/checkout.blade.php
+++ resources/views/frontend/checkout.blade.php
@@ -36,7 +36,9 @@
                         </thead>
                         <tbody>
                         @if(session('cart'))
-                        {{session('cart')}}
+                       {{-- Dump cart session data --}}
+@dump(session('cart'))
+
                             @foreach(session('cart') as $id=>$details)
                             <tr>
                                 <th scope="row">{{$loop->iteration}}</th>
