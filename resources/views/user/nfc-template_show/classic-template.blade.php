036893e7b106c297725e10affa8e1aeb3137bd7c
tawhid12 <tawhid99@gmail.com>
2024-04-02 22:24:37 +0600

    correction

--- resources/views/user/nfc-template_show/classic-template.blade.php
+++ resources/views/user/nfc-template_show/classic-template.blade.php
@@ -7,7 +7,7 @@
         width: calc(100% + 0px);
     }
 </style>
-<div>
+<div class="card p-3">
     <div class="col-md-12">
         <div class="classic_header_image_show">
             @if ($nfc_card->client?->image)
