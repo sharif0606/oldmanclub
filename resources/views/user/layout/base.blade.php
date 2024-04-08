beb7ae74cca2b571cd7137a9218f2da8d61826fb
tawhid12 <tawhid99@gmail.com>
2024-04-08 19:31:53 +0600

    correction

--- resources/views/user/layout/base.blade.php
+++ resources/views/user/layout/base.blade.php
@@ -1207,6 +1207,8 @@ JS libraries, plugins and custom scripts -->
                     $('#editMessage').val(data.message);
                     $('#editPrivacy').val(data.privacy);
                     
+                    // Set the post ID to the input field's data attribute
+                    $('input[data-post-id]').attr('data-post-id', postId);
                     // Open the edit post modal
                     $('#editPostModal').modal('show');
                 },
