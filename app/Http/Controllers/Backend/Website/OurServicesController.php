98519e3408f5adad21e99bf5ed21cd0b2f705221
jasim uddin <jasimuddinm180@gmail.com>
2024-04-24 18:47:25 +0600

    resize image function done

--- app/Http/Controllers/Backend/Website/OurServicesController.php
+++ app/Http/Controllers/Backend/Website/OurServicesController.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers\Backend\Website;
 use App\Models\Backend\Website\OurServices;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Traits\ImageHandleTraits;
+use Intervention\Image\Facades\Image;
+Use File;
 
 class OurServicesController extends Controller
 {
+    use ImageHandleTraits;
     /**
      * Display a listing of the resource.
      */
@@ -35,11 +39,28 @@ class OurServicesController extends Controller
             $ourservices->title = $request->title;
             $ourservices->link = $request->link;
             $ourservices->details = $request->details;
-            if($request->hasFile('image')){
-                $imageName = rand(111, 999).'.'.$request->image->extension();
-                $request->image->move(public_path('uploads/ourservices'),$imageName);
+
+            // if($request->has('image'))
+            //     $ourservices->image=$this->resizeImage($request->image,'uploads/ourservices',true,300,200,false);
+            if ($request->hasFile('image')) {
+                $image = $request->file('image');
+
+                // Set specific size for the image
+                $imageResize = Image::make($image)->resize(300, 300);
+
+                $imageName = rand(111, 999).'.'.$image->extension();
+                $imagePath = public_path('uploads/ourservices/') . $imageName;
+                
+                // Save the resized image
+                $imageResize->save($imagePath);
+
                 $ourservices->image = $imageName;
             }
+            // if($request->hasFile('image')){
+            //     $imageName = rand(111, 999).'.'.$request->image->extension();
+            //     $request->image->move(public_path('uploads/ourservices'),$imageName);
+            //     $ourservices->image = $imageName;
+            // }
             if($ourservices->save()){
                 $this->notice::success('OurServices Successfully Saved');
                 return redirect()->route('ourservice.index');
@@ -65,6 +86,7 @@ class OurServicesController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
+
     public function edit($id)
     {
         $ourservices = OurServices::findOrFail(encryptor('decrypt',$id));
@@ -77,15 +99,42 @@ class OurServicesController extends Controller
     public function update(Request $request,$id)
     {
         try{
+            // dd($request->all());
             $ourservices = OurServices::findOrFail(encryptor('decrypt',$id));
             $ourservices->title = $request->title;
             $ourservices->link = $request->link;
             $ourservices->details = $request->details;
-            if($request->hasFile('image')){
-                $imageName = rand(111, 999).'.'.$request->image->extension();
-                $request->image->move(public_path('uploads/ourservices'),$imageName);
+
+            // $path='uploads/ourservices';
+            // if ($request->hasFile('image')) {
+            //     $this->deleteImage($ourservices->image, $path);
+            //     $imageName = $this->resizeImage($request->file('image'), $path, true, 300, 200, false);
+            //     $ourservices->image = $imageName;
+            // }
+            $path = 'uploads/ourservices';
+
+            if ($request->hasFile('image')) {
+                $image = $request->file('image');
+
+                // Delete the old image
+                $this->deleteImage($ourservices->image, $path);
+
+                // Set specific size for the image
+                $imageResize = Image::make($image)->resize(300, 300);
+
+                $imageName = rand(111, 999) . '.' . $image->extension();
+                $imagePath = public_path("$path/$imageName");
+
+                // Save the resized image
+                $imageResize->save($imagePath);
+
                 $ourservices->image = $imageName;
             }
+            // if($request->hasFile('image')){
+            //     $imageName = rand(111, 999).'.'.$request->image->extension();
+            //     $request->image->move(public_path('uploads/ourservices'),$imageName);
+            //     $ourservices->image = $imageName;
+            // }
             if($ourservices->save()){
                 $this->notice::success('OurServices Successfully Updated');
                 return redirect()->route('ourservice.index');
