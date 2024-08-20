e8f328ba66cd861e3071c24c76fb548610c4aea2
tawhid12 <tawhid99@gmail.com>
2024-08-20 17:40:47 +0600

    image path link correction

--- app/Message.php
+++ app/Message.php
@@ -3,11 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\User\Client;
 class Message extends Model
 {
     protected $fillable = ['from_user', 'to_user', 'message', 'file', 'is_read'];
     public function client(){
-        return $this->hasOne(Client::class ,'from_user','id');
+        return $this->belongsTo(Client::class ,'from_user','id');
     }
 }
