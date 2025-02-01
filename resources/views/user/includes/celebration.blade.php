674623d6c4f3ec7385557ea6bd8b6ca92d6b3d6c
tawhid12 <tawhid99@gmail.com>
2025-02-01 13:38:24 +0600

    correction

--- resources/views/user/includes/celebration.blade.php
+++ resources/views/user/includes/celebration.blade.php
@@ -5,26 +5,27 @@
     </div>
     <div class="card-body">
         @if($online_birthday_users->isEmpty())
-            <p class="text-center text-muted">No online friends have a birthday today.</p>
+        <p class="text-center text-muted">No online friends have a birthday today.</p>
         @else
-            @foreach($online_birthday_users as $client)
-                <div class="mb-3">
-                    <!-- Avatar --> 
-                    <div class="avatar avatar-lg w-100 mb-2 d-flex justify-content-center">
-                        <a href="#!">
-                            <img class="avatar-img rounded-circle" src="{{ asset('public/user/assets/images/avatar/default.jpg') }}" alt="">
-                        </a>
-                    </div>
-                    <!-- Info -->
-                    <div class="ms-2 w-100 text-center">
-                        <h6><a href="#!">{{ $client->fname }} {{ $client->middle_name }} {{ $client->last_name }}</a></h6>
-                        <p class="small mb-0 mb-sm-2">Today is their birthday 🎉</p>
-                    </div>
-                    <div class="position-relative w-100">
-                        <textarea class="form-control pe-4" rows="1" placeholder="Happy birthday dear.."></textarea>
-                    </div>
-                </div>
-            @endforeach
+        @foreach($online_birthday_users as $client)
+        <div class="mb-3">
+            <!-- Avatar -->
+            <div class="avatar avatar-lg w-100 mb-2 d-flex justify-content-center">
+                @if ($client->image)
+                <a href="{{ route('client_by_search', $client->username) }}"><img class="avatar-img rounded-circle" src="{{ asset('public/uploads/client/' . $client->image) }}" alt=""></a>
+                @else
+                @endif
+            </div>
+            <!-- Info -->
+            <div class="ms-2 w-100 text-center">
+                <h6><a href="#!">{{ $client->fname }} {{ $client->middle_name }} {{ $client->last_name }}</a></h6>
+                <p class="small mb-0 mb-sm-2">Today is their birthday 🎉</p>
+            </div>
+            <div class="position-relative w-100">
+                <textarea class="form-control pe-4" rows="1" placeholder="Happy birthday dear.."></textarea>
+            </div>
+        </div>
+        @endforeach
         @endif
     </div>
 </div>
